c0b653a2e72f34943d24d0800870fdf616ac72ef
mr.liu <158442534@qq.com>
2020-11-13 15:17:37 +0800

    完善k8s yaml资源清单

--- src/main/java/com/lion/gateway/ApplicationGateway.java
+++ src/main/java/com/lion/gateway/ApplicationGateway.java
@@ -5,6 +5,8 @@ import com.lion.config.EntityAuditorConfiguration;
 import com.lion.gateway.loadbalancer.LionLoadBalancerClientConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.config.GatewayReactiveLoadBalancerClientAutoConfiguration;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClientConfiguration;
@@ -13,14 +15,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 
+@EnableDiscoveryClient
 @SpringBootApplication ()
 @ComponentScan(value = "com.lion.**", excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {GatewayReactiveLoadBalancerClientAutoConfiguration.class, EntityAuditorConfiguration.class, RestulException.class, LoadBalancerClientConfiguration.class}))
-@EnableDiscoveryClient
 @LoadBalancerClients(defaultConfiguration = {LionLoadBalancerClientConfiguration.class})
 public class ApplicationGateway {
 
     public static void main (String[] args) {
-
         /*
          * new SpringApplicationBuilder(Application.class)
          * .web(WebApplicationType.NONE) .run(args);
